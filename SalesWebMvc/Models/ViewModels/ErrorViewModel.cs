db50f4c920e583fa0f06594b120d6fa035ed7ef0
Throf <mateus.florentino2001@gmail.com>
2021-01-16 17:24:15 -0300

    CRUD Scaffolding

--- SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
+++ SalesWebMvc/Models/ViewModels/ErrorViewModel.cs
@@ -1,7 +1,7 @@
 using System;
 
-namespace SalesWebMvc.Models.ViewModels
-{
+namespace SalesWebMvc.Models
+    { 
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
